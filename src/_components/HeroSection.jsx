ec37d89e589e01188f5d5de580f35dd421d8309a
elias-m25 <elias@m25.space>
2024-05-23 13:36:17 +0200

    stage 1.1 Hader

--- src/_components/HeroSection.jsx
+++ src/_components/HeroSection.jsx
@@ -1,6 +1,8 @@
+"use client";
 import Image from 'next/image';
 import React from 'react';
-
+import { Button, buttonVariants, primary, link, secondary } from '@/components/ui/button';
+import { TypeAnimation } from 'react-type-animation';
 
 const HeroSection = () => {
   return (
@@ -8,18 +10,37 @@ const HeroSection = () => {
       <div className='grid grid-cols-1 sm:grid-cols-12'>
         <div className='col-span-7 place-self-center text-center sm:text-left'>
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-            Hello, I´m Elías
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>
+              Hello, I'm{" "}
+            </span>
+            <TypeAnimation
+            sequence={[
+              "Elías",
+              1000,
+              "Web Developer",
+              1000,
+              "Learning Blockchain",
+              1000,
+
+            ]}
+            wrapper="span"
+            speed={50}
+
+            repeat={Infinity}/>
           </h1>
           <p className='text-white text-base sm:text-lg mb-6  lg:text-xl'>
             A Junior Full Stack Developer who loves solving problems with programming...
           </p>
-          <div className='sm:flex flex flex-col '>
-            <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>
+          <div className=''>
+            <Button className='mb-5'>
               Hire Me
-              </button>
-            <button className='px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3'>
-              Download CV
-              </button>
+            </Button>
+            <Button
+              variant="secondary"
+              className='px-1 py-1 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white '
+            >
+             <span className='block bg-[#121212] hover:bg-slate-800 rounded-full py-2 md:px-auto px-3'>Download CV</span> 
+            </Button>
           </div>
         </div>
         <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import HeroSection from "./../_components/HeroSection";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <div className="container mx-auto px-12 py-4">
+      <div className="container mx-auto px-12 py-4 ">
 
         <HeroSection />
       </div>
