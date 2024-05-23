ee52deec2f2bfa32ba33e3d7d528b1b8d1b52938
elias-m25 <elias@m25.space>
2024-05-23 16:58:43 +0200

    beginning stage 1.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import HeroSection from "./../_components/HeroSection";
-
+import Navbar from "../_components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
+        <Navbar />
       <div className="container mx-auto px-12 py-4 ">
-
         <HeroSection />
       </div>
     </main>
