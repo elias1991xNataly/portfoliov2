10ef4ecbfe817bb80dd854a23a89b58cdace8a91
elias-m25 <elias@m25.space>
2024-05-28 16:19:15 +0200

    stage 1.3 finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import Image from "next/image";
 import HeroSection from "./../componentss/HeroSection";
 import Navbar from "../componentss/Navbar";
+import AboutSection from "../componentss/AboutSection";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-        <Navbar />
+
+      <Navbar />
       <div className="container mt-24 mx-auto px-12 pt-7">
         <HeroSection />
+        <AboutSection />
       </div>
+
     </main>
   );
 }
