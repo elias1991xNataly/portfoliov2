f1ed66e88aef7ca90e09d44dfebb2f48987a8257
elias-m25 <elias@m25.space>
2024-05-30 10:47:19 +0200

    finished 1.4

--- src/componentss/ProjectTag.jsx
+++ src/componentss/ProjectTag.jsx
@@ -6,8 +6,11 @@ const ProjectTag = ({ name, onClick, isSelected }) => {
         : "text-[#ADB7BE] border-slate-600 hover:border-white"
         ;
     return (
-        <button className='rounded-full border-2 border-purple-500 px-6 py-3 text-xl cursor-pointer'>
-            All
+        <button
+            onClick={() => onClick(name)}
+            className={`${buttonStyles} rounded-full border-2  px-6 py-3 text-xl cursor-pointer`}
+        >
+            {name}
         </button>
     )
 }
--- src/componentss/ProjectsSection.jsx
+++ src/componentss/ProjectsSection.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+"use client";
+import React, { useState } from 'react';
 import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
 
 
 const projectsData = [
@@ -9,17 +11,17 @@ const projectsData = [
     description: "Coffee Ecommerce shop",
     image: "/projects/Project_1.png",
     tag: ["All", "Web"],
-    gitUrl:"/",
-    previewUrl:"/"
+    gitUrl: "/",
+    previewUrl: "/"
   },
   {
     id: 2,
     title: "Actors Api",
     description: "Sorting, Adding and Removing",
     image: "/projects/Actors.png",
-    tag: ["All", "Web"],
-    gitUrl:"/",
-    previewUrl:"/"
+    tag: ["All"],
+    gitUrl: "/",
+    previewUrl: "/"
   },
   {
     id: 3,
@@ -27,29 +29,49 @@ const projectsData = [
     description: "",
     image: "/projects/Countries.png",
     tag: ["All", "Web"],
-    gitUrl:"/",
-    previewUrl:"/"
+    gitUrl: "/",
+    previewUrl: "/"
   }
 ]
 
 const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) => 
+    project.tag.includes(tag)
+  )
+
+
+  ;
   return (
     <>
       <h2 className='text-center text-4xl font-bold text-white mt-4'>My Projects</h2>
       <div className='text-white flex flex-row justify-center items-center gap-2 py-6'>
-        <button className='rounded-full border-2 border-purple-500 px-6 py-3 text-xl cursor-pointer'>All</button>
-        <button className='rounded-full border-2 border-blue-500 hover:border-blue-300 px-6 py-3 text-xl cursor-pointer'>Web</button>
+        <ProjectTag
+          name="All"
+          isSelected={tag === "All"}
+          onClick={handleTagChange}
+        />
+        <ProjectTag
+          name="Web"
+          isSelected={tag === "Web"}
+          onClick={handleTagChange}
+        />
       </div>
       <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
-        {projectsData.map((project) => 
-        <ProjectCard
-          key={project.id}
-          title={project.title}
-          description={project.description}
-          imgUrl={project.image}
-          gitUrl={project.gitUrl}
-          previewUrl={project.previewUrl}
-        />)}
+        {filteredProjects.map((project) =>
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            imgUrl={project.image}
+            gitUrl={project.gitUrl}
+            previewUrl={project.previewUrl}
+          />)}
       </div>
     </>
   )
