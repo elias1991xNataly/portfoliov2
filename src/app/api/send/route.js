af9311f3b3bcaba8290ba30466145f5380815e8a
elias-m25 <elias@m25.space>
2024-05-31 16:46:09 +0200

    without resend

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,38 +1,38 @@
-// import { EmailTemplate } from '../../../components/EmailTemplate';
-import { Resend } from 'resend';
-import { NextResponse } from 'next/server'
-import { log } from 'console';
+// // import { EmailTemplate } from '../../../components/EmailTemplate';
+// import { Resend } from 'resend';
+// import { NextResponse } from 'next/server'
+// import { log } from 'console';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
+// const resend = new Resend(process.env.RESEND_API_KEY);
+// const fromEmail = process.env.FROM_EMAIL;
 
 
-// from: 'Elias <eliasalfonso1991@gmail.com>',
-//       to: ['eliasalfonso1990@hotmail.com'],
-//       subject: 'Hello world',
-export async function POST(req, res) {
-    const { body } = await req.json();
-    const { email, subject, message } = body;
-    console.log(body);
-    try {
-        const { data, error } = await resend.emails.send({
-            from: fromEmail,
-            to: ["eliasalfonso1991@gmail.com", email],
-            subject: subject,
-            react: <>
-                <h1>{subject}</h1>
-                <p>Thank you for contacting us!</p>
-                <p>New Message submitted:</p>
-                <p>{message}</p>
-            </>,
-        });
+// // from: 'Elias <eliasalfonso1991@gmail.com>',
+// //       to: ['eliasalfonso1990@hotmail.com'],
+// //       subject: 'Hello world',
+// export async function POST(req, res) {
+//     const { body } = await req.json();
+//     const { email, subject, message } = body;
+//     console.log(body);
+//     try {
+//         const { data, error } = await resend.emails.send({
+//             from: fromEmail,
+//             to: ["eliasalfonso1991@gmail.com", email],
+//             subject: subject,
+//             react: <>
+//                 <h1>{subject}</h1>
+//                 <p>Thank you for contacting us!</p>
+//                 <p>New Message submitted:</p>
+//                 <p>{message}</p>
+//             </>,
+//         });
 
-        if (error) {
-            return Response.json({ error }, { status: 500 });
-        }
+//         if (error) {
+//             return Response.json({ error }, { status: 500 });
+//         }
 
-        return Response.json(data);
-    } catch (error) {
-        return Response.json({ error }, { status: 500 });
-    }
-}
+//         return Response.json(data);
+//     } catch (error) {
+//         return Response.json({ error }, { status: 500 });
+//     }
+// }
--- src/componentss/EmailSection.jsx
+++ src/componentss/EmailSection.jsx
@@ -9,33 +9,33 @@ const EmailSection = () => {
     const [emailSubmitted, setEmailSubmitted] = useState(false);
 
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const data = {
-            email: e.target.email.value,
-            subject: e.target.subject.value,
-            message: e.target.message.value
-        }
-        const JSONdata = JSON.stringify(data);
-        const endpoint = "/api/send";
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
+    //     const data = {
+    //         email: e.target.email.value,
+    //         subject: e.target.subject.value,
+    //         message: e.target.message.value
+    //     }
+    //     const JSONdata = JSON.stringify(data);
+    //     const endpoint = "/api/send";
 
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSONdata,
-        }
-        const response = await fetch(endpoint, options);
-        const resData = await response.json();
+    //     const options = {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSONdata,
+    //     }
+    //     const response = await fetch(endpoint, options);
+    //     const resData = await response.json();
 
-        console.log(resData);
+    //     console.log(resData);
 
-        if (response.status === 200) {
-            console.log('Message sent.');
-            setEmailSubmitted(true)
-        }
-    };
+    //     if (response.status === 200) {
+    //         console.log('Message sent.');
+    //         setEmailSubmitted(true)
+    //     }
+    // };
     return (
         <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative' >
             <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-y-1/2'></div>
@@ -55,7 +55,7 @@ const EmailSection = () => {
                     </Link>
                 </div>
             </div>
-            <div>
+            {/* <div>
                 <form className='flex flex-col' onSubmit={handleSubmit}>
                     <div className='mb-6'>
 
@@ -114,7 +114,7 @@ const EmailSection = () => {
                         </p>
                     )}
                 </form>
-            </div>
+            </div> */}
         </section>
     )
 }
